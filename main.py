2387bddb53d666dae32e60f1c07b6c0b48cf9c43
Zinaida <zinaida.benaoudia@ens.uvsq.fr>
2021-05-11 16:48:48 +0200

    fonctions + variables

---
+++ main.py
@@ -0,0 +1,34 @@
+
+#fonctions à faire :
+  #(OK) # generate -> qui genere et affiche le plateau à l'aide d'un fichier txt 
+  #(OK) # show_target -> qui affiche la cible 
+  #(OK) # show_robots -> qui affiche les robots 
+  #(OK) # keyboard -> qui permet de se déplacer (verticalement et horizontalement)
+  #(OK) # deplacement -> qui permet de déplacer un robot lorsqu'on clique dessus
+        # maj_pos -> qui permet de réinisialiser la position du robot 
+        # clr_mvmt -> qui permet d'affichet les possibles deplacements d'un robot lorsque l'on clique dessus
+  #(OK) # click -> click
+  #(OK) # position -> qui permet de return une position d'une case  
+        # win -> qui permet d'afficher un message lorsque l'un des robots gagne 
+        # restart -> qui permet de recommencer une partie 
+        # cpt -> qui permet de compter le nombre de deplacements qu'un robot a fait 
+        # save -> qui permet de sauvegarder une partie
+        # return -> qui permet de revenir en arriere (d'annuler son deplcement)
+  #(OK) # obstacle -> qui permet d'arreter le robot lorsqu'il est devant un obstacle 
+        # Unfo -> qui permet de revenir en arrière 
+
+
+#constantes :
+        # height = 640
+        # width = 640
+        # size = 16
+        # cote = 40 (HEIGHT//16)
+#variable :
+        # tableau = None
+        # target = None
+        # pos_robot = []
+        # robots = []
+#bouton :
+        # btn_save
+        # btn_load
+
