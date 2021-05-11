a438b58304212b560c33813db33501929f7cf151
Zinaida <zinaida.benaoudia@ens.uvsq.fr>
2021-05-11 17:48:53 +0200

    prog avec noms des variables en anglais

--- version0.1.py
+++ version0.1.py
@@ -2,12 +2,12 @@ import tkinter as tk
 import random 
 
 # Constante
-HEIGHT = 640
-WIDTH  = 640
-SIZE = 16
-COTE = HEIGHT // SIZE
+height = 640
+width  = 640
+size = 16
+side = height // size
 
-tableau = None
+table = None
 target = None
 pos_robot = []
 robots = []
@@ -16,19 +16,19 @@ dy = 0
 
 
 def grid():
-    for i in range(0,HEIGHT,COTE):
-        for j in range(0,WIDTH,COTE):
-            canvas.create_rectangle(i,j,i+COTE,j+COTE,fill="white")
+    for i in range(0,height,side):
+        for j in range(0,width,side):
+            canvas.create_rectangle(i,j,i+side,j+side,fill="white")
 
 
 def generate():
     """ affiche walls + pos target + pos robots"""
-    global tableau, target, pos_robot
-    fichier = open("Board.txt", "r")
+    global table, target, pos_robot
+    file = open("Board.txt", "r")
     nb_line = 0
-    for line in fichier:
+    for line in file:
         if 4 <= nb_line < 36:
-            tableau = [line.split() for line in fichier]
+            table = [line.split() for line in file]
                         
         if nb_line == 0:
             target = line.split()
@@ -52,33 +52,33 @@ def generate():
         nb_line +=1
 
 def show_walls():
-    for i in range(len(tableau)):
-        for j in range(len(tableau[i])):
-            if tableau[i][j] == "2": # -
-                canvas.create_line(i//2*COTE, j*COTE, (i//2+1)*COTE, j*COTE, fill="black", width=5)
+    for i in range(len(table)):
+        for j in range(len(table[i])):
+            if table[i][j] == "2": # -
+                canvas.create_line(i//2*side, j*side, (i//2+1)*side, j*side, fill="black", width=5)
 
-            if tableau[i][j] == "1": # |
-                canvas.create_line(i//2*COTE, j*COTE, i//2*COTE, (j+1)*COTE, fill="black", width=5)
+            if table[i][j] == "1": # |
+                canvas.create_line(i//2*side, j*side, i//2*side, (j+1)*side, fill="black", width=5)
 
 def show_robots():
     global robot
     x = int(pos_robot[0][0])
     y = int(pos_robot[0][1])
-    bleu = canvas.create_oval(x*COTE+5, y*COTE+5, x*COTE+35, y*COTE+35, fill = "blue")
+    blue = canvas.create_oval(x*side+5, y*side+5, x*side+35, y*side+35, fill = "blue")
 
     x = int(pos_robot[1][0])
     y = int(pos_robot[1][1])
-    red = canvas.create_oval(x*COTE+5, y*COTE+5, x*COTE+35, y*COTE+35, fill = "red")
+    red = canvas.create_oval(x*side+5, y*side+5, x*side+35, y*side+35, fill = "red")
 
     x = int(pos_robot[2][0])
     y = int(pos_robot[2][1])
-    green = canvas.create_oval(x*COTE+5, y*COTE+5, x*COTE+35, y*COTE+35, fill = "green")
+    green = canvas.create_oval(x*side+5, y*side+5, x*side+35, y*side+35, fill = "green")
 
     x = int(pos_robot[3][0])
     y = int(pos_robot[3][1])
-    yellow = canvas.create_oval(x*COTE+5, y*COTE+5, x*COTE+35, y*COTE+35, fill = "yellow")
+    yellow = canvas.create_oval(x*side+5, y*side+5, x*side+35, y*side+35, fill = "yellow")
 
-    robots.append(bleu)
+    robots.append(blue)
     robots.append(red)
     robots.append(green)
     robots.append(yellow)
@@ -86,7 +86,46 @@ def show_robots():
 def show_target():
     x = int(target[0])
     y = int(target[1])
-    target_b = canvas.create_rectangle(x*COTE+10, y*COTE+10, x*COTE+30, y*COTE+30, fill="blue")
+    target_b = canvas.create_rectangle(x*side+10, y*side+10, x*side+30, y*side+30, fill="blue")
+
+ 
+def show_targets():
+    pass
+    """Création des cibles + définir leur position """
+    global targets, pos_target
+
+    n = random.randrange(40, 600, 40)
+    m = random.randrange(40, 600, 40)
+
+    x = random.randrange(40, 600, 40)
+    y = random.randrange(40, 600, 40)
+
+    u = random.randrange(40, 600, 40)
+    v = random.randrange(40, 600, 40)
+
+    a = random.randrange(40, 600, 40)
+    b = random.randrange(40, 600, 40)
+
+    Blue = canvas.create_rectangle(n+5, m+5, n+35, m+35, fill="blue")
+    Red = canvas.create_rectangle(x+5, y+5, x+35, y+35, fill="red")
+    Yellow = canvas.create_rectangle(u+5, v+5, u+35, v+35, fill="yellow")
+    Green = canvas.create_rectangle(a+5, b+5, a+35, b+35, fill="green")
+
+    position_bleu = (n//side, m//side)
+    position_red = (x//side, y//side)
+    position_yellow = (u//side, v//side)
+    position_green = (a/side, b//side)
+
+    pos_target.append(position_bleu)
+    pos_target.append(position_red)
+    pos_target.append(position_yellow)
+    pos_target.append(position_green)
+
+    targets.append(Blue)
+    targets.append(Red)
+    targets.append(Yellow)
+    targets.append(Green)
+
     
 
 def stop(robot,stop_x,stop_y):
@@ -144,7 +183,7 @@ root = tk.Tk()
 
 
 # Creation des widgets
-canvas = tk.Canvas(root,height=HEIGHT, width=WIDTH)
+canvas = tk.Canvas(root,height=height, width=width)
 bouton = tk.Button(root, text="Génération terrain")
 
 
